cebf4d8b8c0154227184c70c8631c8b2d10c8104
Jesse Okolie <Adsired31024sure@gmail.com>
2023-05-11 12:15:04 -0400

    Changed File Path

--- helper/openai_api.py
+++ helper/openai_api.py
@@ -1,7 +1,7 @@
 import os
-
 import sys
-
+import urllib.request
+from pathlib import Path
 from gpt_index import (
     SimpleDirectoryReader,
     GPTSimpleVectorIndex,
@@ -9,25 +9,24 @@ from gpt_index import (
     LLMPredictor,
     PromptHelper,
 )
-
 from langchain import OpenAI
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-path = "C:\\Users\\USER\\Desktop\\SOFTWARE DEVELOPMENT\\EdaTeleBot\\Information"
+def construct_index(url):
+    # download file from url and save it locally
+    filename = "file.txt"
+    urllib.request.urlretrieve(url, filename)
 
+    # set path variable to the local file path
+    path = str(Path(filename).resolve())
 
-def construct_index(path):
     max_input_size = 4096
-
     tokens = 256
-
     max_chunk_overlap = 20
-
     chunk_size_limit = 600
 
     # define LLM
@@ -55,8 +54,8 @@ def construct_index(path):
     return vectorIndex
 
 
-vectorIndex = construct_index(path)
-
+url = "https://www.dropbox.com/sh/8pa2z55h3c03n9v/AABR4OUDfZSkeE_Mg3BgBjVIa?dl=1"
+vectorIndex = construct_index(url)
 
 def answerMe(prompt: str) -> dict:
     if not os.path.isfile("index.json"):
